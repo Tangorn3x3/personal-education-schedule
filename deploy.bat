a6e12d7a1cc7d7be94800b3c753fdf46c443c5b6
mbeliakov <mbeliakov@nurtelecom.kg>
2023-11-06 13:00:32 +0600

    deploy build

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
 npm run generate --spa &&^
-gcloud builds submit --tag gcr.io/snippeter-frontend/snippeter-frontend --project snippeter-frontend &&^
-gcloud run deploy snippeter-frontend --image gcr.io/snippeter-frontend/snippeter-frontend --platform managed --region europe-west1 --allow-unauthenticated --project snippeter-frontend --port 80
+gcloud config set project personaleducationschedule &&^
+gcloud builds submit --tag gcr.io/maxkain050589/personal-education-schedule --project personaleducationschedule &&^
+gcloud run deploy personal-education-schedule --image gcr.io/maxkain050589/personal-education-schedule --platform managed --region europe-west1 --allow-unauthenticated --project personaleducationschedule --port 80
